67df2f51e907f4c79a382957c963603f826eb5a8
Jack Lindgren <38013685+jacklindgren24@users.noreply.github.com>
2018-11-30 16:58:33 +0100

    FMOD intregration, movement update

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -2,44 +2,40 @@
 
 public class PlayerMovement : MonoBehaviour {
 
-    public float baseAcceleration = 6;
-    public float sprintAcceleration = 1.5f;
-    public float maxBaseSpeed = 8;
-    public float maxSprintSpeed = 12;
-    public float stopping = 10;
+    public float baseSpeed = 8;
+    public float sprintSpeed = 12;
+    public float jumpHeight = 2;
+
+    const float shell = 0.02f;
 
     Rigidbody rb;
+    CapsuleCollider coll;
 
 	void Start ()
     {
         rb = GetComponent<Rigidbody>();
+        coll = GetComponent<CapsuleCollider>();
 	}
 	
 	void FixedUpdate ()
     {
+        bool isGrounded = Physics.Raycast(transform.position, -transform.up, coll.height + shell);
+
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
 
-        rb.drag = 0;
-
         if (Input.GetButton("Sprint"))
         { // Sprinting.
-            rb.AddForce(new Vector3(x, 0, z) * sprintAcceleration, ForceMode.Impulse);
-            rb.velocity = new Vector3
-             (Mathf.Clamp(rb.velocity.x, -maxSprintSpeed, maxSprintSpeed), rb.velocity.y, Mathf.Clamp(rb.velocity.z, -maxSprintSpeed, maxSprintSpeed));
+            rb.MovePosition(rb.position + new Vector3(x, 0, z) * sprintSpeed * Time.fixedDeltaTime);
         }
         else
-        { // Not sprinting.
-            rb.AddForce(new Vector3(x, 0, z) * baseAcceleration, ForceMode.Impulse);
-            rb.velocity = new Vector3
-             (Mathf.Clamp(rb.velocity.x, -maxBaseSpeed, maxBaseSpeed), rb.velocity.y, Mathf.Clamp(rb.velocity.z, -maxBaseSpeed, maxBaseSpeed));
+        { // Normal.
+            rb.MovePosition(rb.position + new Vector3(x, 0, z) * baseSpeed * Time.fixedDeltaTime);
         }
 
-        
-
-        if (x == 0 && z == 0)
-        { // Increase drag when not moving.
-            rb.drag = stopping;
+        if (isGrounded && Input.GetButtonDown("Jump"))
+        { // Jump.
+            rb.AddForce(transform.up * jumpHeight, ForceMode.VelocityChange);
         }
     }
 }
