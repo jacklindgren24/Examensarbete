b76ddbfce0c785c97e2e039710601a5ddf4fffd7
Jack Lindgren <38013685+jacklindgren24@users.noreply.github.com>
2018-12-01 23:30:47 +0100

    Added mouselook

---
+++ Assets/Scripts/Player/MouseLook.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class MouseLook : MonoBehaviour {
+
+    public float sensitivityX = 1.0f;
+    public float sensitivityY = 1.0f;
+    public float maxAngle = 90;
+    public float minAngle = -90;
+
+    float x = 0;
+    float y = 0;
+    Transform player;
+
+	void Start ()
+    {
+        player = transform.parent;
+	}
+	
+	void Update ()
+    {
+        Vector2 dir = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"));
+
+        x += dir.x * sensitivityX;
+        y += dir.y * sensitivityY;
+        y = Mathf.Clamp(y, minAngle, maxAngle);
+
+        transform.localEulerAngles = new Vector2(-y, 0); // Rotate camera vertically.
+        player.localEulerAngles = new Vector2(0, x); // Rotate player horizontally.
+    }
+}
--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -2,35 +2,35 @@
 
 public class PlayerMovement : MonoBehaviour {
 
-    public float baseSpeed = 8;
-    public float sprintSpeed = 12;
+    public float baseSpeed = 6;
+    public float sprintSpeed = 10;
     public float jumpHeight = 2;
 
     const float shell = 0.02f;
 
     Rigidbody rb;
-    CapsuleCollider coll;
+    float height;
 
 	void Start ()
     {
         rb = GetComponent<Rigidbody>();
-        coll = GetComponent<CapsuleCollider>();
+        height = transform.localScale.y;
 	}
 	
 	void FixedUpdate ()
     {
-        bool isGrounded = Physics.Raycast(transform.position, -transform.up, coll.height + shell);
+        bool isGrounded = Physics.Raycast(transform.position, -transform.up, height + shell);
 
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
 
         if (Input.GetButton("Sprint"))
         { // Sprinting.
-            rb.MovePosition(rb.position + new Vector3(x, 0, z) * sprintSpeed * Time.fixedDeltaTime);
+            rb.MovePosition(rb.position + transform.localRotation * new Vector3(x, 0, z) * sprintSpeed * Time.fixedDeltaTime);
         }
         else
         { // Normal.
-            rb.MovePosition(rb.position + new Vector3(x, 0, z) * baseSpeed * Time.fixedDeltaTime);
+            rb.MovePosition(rb.position + transform.localRotation * new Vector3(x, 0, z) * baseSpeed * Time.fixedDeltaTime);
         }
 
         if (isGrounded && Input.GetButtonDown("Jump"))
