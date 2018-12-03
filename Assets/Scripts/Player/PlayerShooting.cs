249af6d88bb1b13e47ecc4989a899ad4275c61b2
Jack Lindgren <38013685+jacklindgren24@users.noreply.github.com>
2018-12-03 14:07:57 +0100

    Shooting update

---
+++ Assets/Scripts/Player/PlayerShooting.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class PlayerShooting : MonoBehaviour {
+
+    public GameObject bulletPrefab;
+    public Transform bulletSpawn;
+    public float lifetime = 2;
+    public float velocity = 50;
+    public float cooldown = 0.2f;
+
+    float timer = 0;
+
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        if (Input.GetButtonDown("Fire1") && timer > cooldown) // Fire.
+        {
+            Fire();
+        }
+    }
+
+    void Fire()
+    {
+        timer = 0;
+
+        GameObject bullet = Instantiate(bulletPrefab, bulletSpawn.position, bulletSpawn.rotation);
+        bullet.GetComponent<Rigidbody>().velocity = bulletSpawn.transform.forward * velocity;
+        Destroy(bullet, lifetime);
+    }
+}
