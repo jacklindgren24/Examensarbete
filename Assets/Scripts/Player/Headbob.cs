34a9c733afe32916116712dfeb0fe66c7dcf7d55
Jack Lindgren <38013685+jacklindgren24@users.noreply.github.com>
2019-03-06 16:50:18 +0100

    Adjusted head bob, removed player mesh

--- Assets/Scripts/Player/Headbob.cs
+++ Assets/Scripts/Player/Headbob.cs
@@ -2,9 +2,10 @@
 
 public class Headbob : MonoBehaviour {
 
-    public float bobbingSpeedBase = 0.18f;
-    public float bobbingSpeedSprint = 0.288f;
-    public float bobbingAmount = 0.2f;
+    public float bobbingSpeedBase = 0.145f;
+    public float bobbingSpeedSprint = 0.24f;
+    public float bobbingAmountBase = 0.08f;
+    public float bobbingAmountSprint = 0.11f;
     public float midpoint = 1;
 
     float timer = 0;
@@ -43,7 +44,10 @@ public class Headbob : MonoBehaviour {
 
             if (waveslice != 0)
             {
-                float translateChange = waveslice * bobbingAmount;
+                float translateChange = pm.isSprinting
+                    ? waveslice * bobbingAmountSprint
+                    : waveslice * bobbingAmountBase;
+
                 float totalAxes = Mathf.Abs(horizontal) + Mathf.Abs(vertical);
                 totalAxes = Mathf.Clamp(totalAxes, 0.0f, 1.0f);
                 translateChange = totalAxes * translateChange;
